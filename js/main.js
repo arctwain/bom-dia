f4f4cfcd54810b386aaa6b2911a382eae867226b
Tina Anastopoulos <arctwain@gmail.com>
2017-05-29 21:21:04 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 70) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 800 );
+        event.preventDefault();
+    });
+});
